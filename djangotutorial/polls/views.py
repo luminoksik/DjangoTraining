d7c07409a6d98fdbcb675e68fa2e7edb777b7f9b
Robert <lolowy3@interia.pl>
2025-03-29 19:07:18 +0100

    part3: 404 handling

--- djangotutorial/polls/views.py
+++ djangotutorial/polls/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Question
+
 
 
 def index(request):
@@ -9,8 +10,8 @@ def index(request):
     return render(request, "polls/index.html", context)
 
 def detail(request, question_id):
-    response = "You are looking at question %s."
-    return HttpResponse(response % question_id)
+    question = get_object_or_404(Question, pk=question_id)
+    return render(request, "polls/detail.html", {"question": question})
 
 def results(request, question_id):
     response = "You are looking at the results of question %s."
